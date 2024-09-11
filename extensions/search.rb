9d86c448babfc1ba59bde75acf9005975d390db5
Kritphong Mongkhonvanit <kritphong@u.northwestern.edu>
2024-09-10 19:50:18 -0500

    Fix crash when guests try to search for something

--- extensions/search.rb
+++ extensions/search.rb
@@ -6,21 +6,52 @@ module SearchMultilingualExtension
 
     if Multilingual::ContentLanguage.topic_filtering_enabled
       user = @guardian.user
+      locale = I18n.locale.to_s
 
-      existing_tags_bare_sql =
-        "(select tag_id from topic_tags where topic_tags.topic_id = posts.topic_id)"
-      content_language_tag_ids_sql = <<~SQL
-        (
-          select t.id
-          from user_custom_fields ucf
-          inner join tags t
-          on ucf.value = t.name
-          where ucf.name = 'content_languages' and ucf.user_id = #{user.id}
-        )
+      existing_tags_bare_sql = <<~SQL
+          (
+            select tag_id
+            from topic_tags
+            where topic_tags.topic_id = posts.topic_id
+          )
       SQL
 
-      content_languages_filter =
-        "exists (#{content_language_tag_ids_sql} intersect #{existing_tags_bare_sql})"
+      if user
+        content_language_tag_ids_sql = <<~SQL
+          (
+            select t.id
+            from user_custom_fields ucf
+            inner join tags t
+            on ucf.value = t.name
+            where ucf.name = 'content_languages' and ucf.user_id = #{user.id}
+          )
+        SQL
+      else
+        locale = I18n.locale.to_s
+
+        raise "Malformed locale" unless /^[a-z]{2}(_[A-Z]{2})?$/.match?(locale)
+
+        content_languages = locale =~ /^zh(_[A-Z]{2})?$/ ?
+                            [locale] :
+                            [locale, "en"].uniq
+
+        content_language_tag_ids_sql = <<~SQL
+          (
+            select id
+            from tags
+            where name in (#{content_languages.map{|x| "'#{x}'"}.join(",")})
+          )
+        SQL
+      end
+
+      content_languages_filter = <<~SQL
+        exists
+        (
+          #{content_language_tag_ids_sql}
+          intersect
+          #{existing_tags_bare_sql}
+        )
+      SQL
 
       posts = posts.where(content_languages_filter)
     end
